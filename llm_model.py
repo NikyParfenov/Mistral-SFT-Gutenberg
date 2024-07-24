a519f527a8b03467413f916b4a736abc4f40e020
Nikita <nikyparfenov@gmail.com>
2024-07-25 00:06:11 +0200

    bug fix

--- llm_model.py
+++ llm_model.py
@@ -8,30 +8,33 @@ from serpersearch import run_google_search
 
 class FTMistral:
     def __init__(self):
-        adapter_model = "ft_model/mistral-gutenberg-books-finetune"
-        base_model_id = "base_model/Mistral-7B-v0.1"
+        try:
+            adapter_model = "ft_model/mistral-gutenberg-books-finetune"
+            base_model_id = "base_model/Mistral-7B-v0.1"
 
-        bnb_config = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_compute_dtype=torch.bfloat16
-        )
+            bnb_config = BitsAndBytesConfig(
+                load_in_4bit=True,
+                bnb_4bit_use_double_quant=True,
+                bnb_4bit_quant_type="nf4",
+                bnb_4bit_compute_dtype=torch.bfloat16
+            )
 
-        base_model = AutoModelForCausalLM.from_pretrained(
-            base_model_id, 
-            quantization_config=bnb_config, 
-            device_map="auto",
-            trust_remote_code=True,
-        )
+            base_model = AutoModelForCausalLM.from_pretrained(
+                base_model_id, 
+                quantization_config=bnb_config, 
+                device_map="auto",
+                trust_remote_code=True,
+            )
 
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            base_model_id,
-            add_bos_token=True,
-        )
+            self.tokenizer = AutoTokenizer.from_pretrained(
+                base_model_id,
+                add_bos_token=True,
+                trust_remote_code=True,
+            )
 
-        self.ft_model = PeftModel.from_pretrained(base_model, adapter_model)
-
+            self.ft_model = PeftModel.from_pretrained(base_model, adapter_model)
+        except Exception as e:
+            print(e)
 
     def run_llm(self, prompt, use_google_search=True):
 
